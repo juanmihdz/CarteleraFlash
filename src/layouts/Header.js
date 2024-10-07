fb5b2c5abed6e73ccfde33242a7da4d372681479
karlos-hr <dev.carloshrios@gmail.com>
2024-10-07 15:39:57 +0100

    style: alineamiento

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -8,7 +8,7 @@ export default function Header() {
 
   return (
     <nav class="flex items-end flex-col md:items-center justify-end md:justify-center p-4 backdrop-blur-lg border-neutral-700 border-b">
-      <span class="absolute left-2 top-3 text-2xl">
+      <span class="absolute left-2 md:top-3 mt-1 top-4 text-2xl">
         Cartelara Flash
       </span>
       <button onClick={toggleClass} class="py-2 right-2 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"><span class="sr-only">Open main menu</span><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" aria-hidden="true" class="h-6 w-6 shrink-0" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg></button>
