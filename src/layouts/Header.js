5e80373ca6d9d1f9fa60083bbbb66a8fa8d57f5f
juanmihdz <juanmiguelhernandezgomez@gmail.com>
2024-10-16 17:40:53 +0200

    refactor: title

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -31,7 +31,7 @@ export default function Header() {
       <nav className='h-fit mb-10 sticky top-0 z-10'>
         <section className="flex justify-between flex-wrap items-center md:justify-start p-4 lg:gap-20 gap-4 backdrop-blur-lg">
           <h1 className="pl-8 text-4xl font-semibold">
-            Cartelara Flash
+            Cartelera Flash
           </h1>
           <button onClick={toggleClass} className="py-2 right-2 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden">
             <svg fill="currentColor" viewBox="0 0 448 512" aria-hidden="true" className="h-6 w-6 shrink-0" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path>
